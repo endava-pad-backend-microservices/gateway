ac6f684a0bbfb824fff5f94fa03d3fbf7cbf1a4d
ayancovich <ayancovich@endava.com>
2019-05-15 16:35:51 -0300

    Added swagger 2 to gateway

--- src/main/java/com/endava/bean/SecurityConstants.java
+++ src/main/java/com/endava/bean/SecurityConstants.java
@@ -3,4 +3,8 @@ package com.endava.bean;
 public class SecurityConstants {
 	public static final String TOKEN_HEADER = "token";
 	public static final String [] UNSECURED_URLS = {"/auth/login"};
+	public static final String ENDPOINT_SWAGGER = "/v2/api-docs";
+	public static final String VERSION_SWAGGER = "2.0";
+	public static final String ZUUL_PREFIX ="/api/";
+	public static final String GATEWAY_NAME ="gateway";
 }
